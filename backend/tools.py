80626320cba5e2f657089ab933b888d6018c0266
klaubeer <klaubeer@gmail.com>
2026-03-05 09:13:18 -0300

    Delete backend/tools.py

--- backend/tools.py
+++
@@ -1,9 +0,0 @@
-# backend/tools.py
-
-def gerador_imagem(estilo_post, descricao_post):
-
-    return {
-        "status": "ok",
-        "estilo": estilo_post,
-        "descricao": descricao_post
-    }
