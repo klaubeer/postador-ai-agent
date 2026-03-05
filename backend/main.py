350a2ac7c3a86af1a4ed729c527db20b27c8b91f
Klauber Fischer <klaubeer@gmail.com>
2026-03-05 18:21:37 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from backend.agent_graph import graph
-from backend.planner import planner
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.agent_graph import graph
+from backend.planner import planner
+
 app = FastAPI()
-
-state = {
-    "objetivo": None,
-    "plataforma": None,
-    "tema": None,
-    "publico": None
-}
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,17 +15,36 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# memória de sessões
+sessions = {}
+
 
 class ChatRequest(BaseModel):
     message: str
+    session_id: str
+
+
+def get_session_state(session_id):
+
+    if session_id not in sessions:
+        sessions[session_id] = {
+            "objetivo": None,
+            "plataforma": None,
+            "tema": None,
+            "publico": None
+        }
+
+    return sessions[session_id]
 
 
 @app.post("/post")
 def create_post(req: ChatRequest):
 
-    global state
+    state = get_session_state(req.session_id)
 
     decision, state = planner(req.message, state)
+
+    sessions[req.session_id] = state
 
     if decision["action"] == "ask_user":
         return {
