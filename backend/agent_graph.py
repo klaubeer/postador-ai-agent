b8a4a315f05b51035cc2561086d0e55e076113cb
Klauber Fischer <klaubeer@gmail.com>
2026-03-05 18:11:09 -0300

    Update agent_graph.py

--- backend/agent_graph.py
+++ backend/agent_graph.py
@@ -2,7 +2,6 @@ from langgraph.graph import StateGraph, END
 from backend.state import AgentState
 
 from backend.nodes import (
-    extract_briefing,
     generate_ideas,
     select_best_idea,
     generate_caption,
@@ -13,7 +12,6 @@ from backend.nodes import (
 
 builder = StateGraph(AgentState)
 
-builder.add_node("briefing", extract_briefing)
 builder.add_node("ideas", generate_ideas)
 builder.add_node("select", select_best_idea)
 builder.add_node("caption", generate_caption)
@@ -21,9 +19,8 @@ builder.add_node("image_prompt", generate_image_prompt)
 builder.add_node("hashtags", generate_hashtags)
 builder.add_node("format", format_post)
 
-builder.set_entry_point("briefing")
+builder.set_entry_point("ideas")
 
-builder.add_edge("briefing", "ideas")
 builder.add_edge("ideas", "select")
 builder.add_edge("select", "caption")
 builder.add_edge("caption", "image_prompt")
